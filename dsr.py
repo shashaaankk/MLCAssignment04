788cab6e874e23dce4d8a066a11cb7810634e2b3
shashaaankk <54775855+shashaaankk@users.noreply.github.com>
2024-06-22 17:52:01 +0200

    dsr updated

--- dsr.py
+++ dsr.py
@@ -1,64 +1,83 @@
 import socket
 if __name__=="__main__":
-        sequence_id = 0
-        my_IP = 64
-        data_packet = ""
-        sequences_served = False
-        # Create the socket
-        socket_send_uni = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # Make the socket multicast-aware, and set TTL.
-        socket_send.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 20) # Change TTL (=20) to suit
-                # Create the socket
-        socket_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # Set some options to make it multicast-friendly
-        socket_recv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sequence_id = 0     # Sequence ID
+        my_IP = 64          # Last byte of WiFi IP of the current node #CHANGE HERE
+        data_packet = ""    # Data to be sent
+        myRouteDict = {}    # Dictionary stores the arriving routes
+
+        # Creating datagram sockets (UDP)
+        socket_send_uni = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Sending via unicast
+        socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      # Forward path 
+        socket_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+
+        # Making the socket multicast-aware, and set TTL.
+        socket_send.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)   # Broadcasting, Option changed to 1 
+        socket_recv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # Bind to the address and port combination that is in use
         try:
                 socket_recv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         except AttributeError:
-                pass # Some systems don't support SO_REUSEPORT
-        socket_recv.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 20)
-        socket_recv.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 1)
+                pass 
 
-        # Bind to the port
-        socket_recv.bind(('', 5005))
+        socket_recv.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 20)  # Multicast with TTL 20
+        socket_recv.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 1)  # Receiving own multicast message
+
+        socket_recv.bind(('', 5005))        # Binding to our port
         socket_recv.settimeout
         while True:
                 data = b""
                 try:
-                        data = socket_recv.recv(1024)
-                        addresses = data[3:]
-                        print("addresses: " + str([b for b in data]))
-                        if sequence_id != data[0]:
+                        data = socket_recv.recv(1024)                      # Receiving 1024 bytes
+                        addresses = data[3:]                               # Last Byte of address
+                        print("Data frame: " + str([b for b in data]))
+                        if sequence_id < data[0]:                          # Acceptence check
+                                # Transmission
                                 sequence_id = data[0]
-                                if data[1] > my_IP and not data[2] :
+                                if data[1] != my_IP and data[2] == 0 :     # data[1]: Last byte of Destination IP; data[2] == 0-> Broadcast; data[2] == 1-> RREP; data[2] == 2-> Using obtained route;
                                         print("Broadcasting packet with squence number"+ str(sequence_id))
-                                        socket_send.sendto(data+bytearray([my_IP]),(("129.69.210.255",5005)))
-                                elif data[1] > my_IP and data[2] and not sequences_served:
-                                        socket_send.close()
-                                        print("RREP"+ str(sequence_id))
-                                        socket_send_uni.sendto(data,(("129.69.210."+str(addresses[addresses.index(my_IP)+1]),5005)))
+                                        socket_send.sendto(data+bytearray([my_IP]),(("192.168.210.255",5005))) # Broadcast
+                                elif data[1] != my_IP and data[2] == 1 :   # Route Reply via unicast
+                                        print("RREP"+ str(sequence_id))    
+                                        myRouteDict.put(int(addresses[-1]),addresses)
+                                        socket_send_uni.sendto(data,(("192.168.210."+str(addresses[addresses.index(my_IP)-1]),5005)))
+                                elif data[1] != my_IP and data[2] == 2:    # Data Transfer
+                                       print("Data Transfer"+ str(sequence_id))
+                                       addr_lst = myRouteDict.get(data[1])
+                                       socket_send_uni.sendto(data,(("192.168.210."+str(addr_lst[addr_lst.index(my_IP)+1]),5005))) # Data Transfer 
+                                # Reception         
                                 else:
-                                    if data[2]:
-                                        print("creating entry")
-                                        print("addresses: " + str([b for b in addresses]))
-                                        sequence_id+=1
-                                        rrep_flag = 0
-                                        socket_send_uni.sendto(bytearray([sequence_id,int(addresses[-1]),rrep_flag,data_packet]),(("129.69.210."+str(addresses[addresses.index(my_IP)+1]),5005)))
+                                    if data[2]==1: #RREP Reception, Store address and Send Data
+                                        print("Creating Entry")
+                                        #print("Addresses: " + str([b for b in addresses]))
+                                        myRouteDict[int(addresses[-1])] = addresses
+                                        #print(myRouteDict)                                    
+                                        for dest in myRouteDict:
+                                            route = myRouteDict[dest]
+                                            formatted_route = ", ".join(f'"{r}"' for r in route) 
+                                            print(f'{dest} : [{formatted_route}]')
+                                        sequence_id+=1 # Prepare to send Data
+                                        rrep_flag = 2  # prepare to send Data
+                                        #print(int(addresses[-1]))
+                                        socket_send_uni.sendto(bytearray([sequence_id,int(addresses[-1]),rrep_flag])+data_packet.encode(),(("192.168.210."+str(addresses[addresses.index(my_IP)+1]),5005))) #Data
+                                    elif data[2]==2: #Data Receiption
+                                        print("Recieved data: "+addresses.decode())
                                     else:
-                                        if not sequences_served:
-                                            sequences_served = True
-                                            sequence_id+=1
-                                            socket_send_uni.sendto(bytearray([sequence_id,int(addresses[0]),1,my_IP])+addresses[::-1],(("129.69.210."+str(addresses[-1]),5005)))
-                        else:
-                                print("Rejected packet with sequence number"+ str(sequence_id))
+                                        print("starting rrep") # Request Received, Sending RREP via Unicast
+                                        sequence_id+=1
+                                        socket_send_uni.sendto(bytearray([sequence_id,int(addresses[0]),1])+addresses+bytearray([my_IP]),(("192.168.210."+str(addresses[-1]),5005)))
+                        else:               
+                                print("Rejected packet with sequence number "+ str(sequence_id))
                 except socket.timeout:
                         pass
                 except KeyboardInterrupt:
                         destination = int(input("Enter Destination IP last byte: "))
-                        data_packet = str(input("Enter 1 byte of data for the packet: "))
+                        data_packet = str(input("Enter 1 byte of data for the packet: "))  
                         sequence_id+=1
-                        rrep_flag = 0
-                        sequences_served  = False
-                        socket_send.sendto(bytearray([sequence_id,destination,rrep_flag,my_IP]),(("129.69.210.255",5005)))
+                        if myRouteDict.get(destination) is None:
+                                rrep_flag = 0
+                                socket_send.sendto(bytearray([sequence_id,destination,rrep_flag,my_IP]),(("192.168.210.255",5005)))
+                        else:
+                                rrep_flag = 2 
+                                addr_lst = myRouteDict.get(destination)
+                                socket_send_uni.sendto(bytearray([sequence_id,int(addr_lst[-1]),rrep_flag])+data_packet.encode(),(("192.168.210."+str(addr_lst[addr_lst.index(my_IP)+1]),5005)))
+                          
